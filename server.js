087b36bbc66f012925c5056759cf0c328b303694
Yong Lin Wang <callmekungfu@users.noreply.github.com>
2019-03-20 18:14:53 -0400

    added cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
+const cors = require('cors');
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -27,6 +28,7 @@ db.on('error', console.error.bind(console, 'MONGO ERR: '));
 
 app.use(express.static(__dirname));
 app.use(bodyparser.json());
+app.use(cors());
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
