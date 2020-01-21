42105321e93b0797daf75b1bd589e88ce2d852ab
Yong Lin Wang <wangyonglin1999@gmail.com>
2020-01-21 18:42:12 -0500

    Updates

---
+++ precache-manifest.c009400da7cbe4782a78d2e2b8b58cd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f55c8a1cc1fc2e16d3674579be831493",
+    "url": "/youtube-mirrorify/index.html"
+  },
+  {
+    "revision": "6be0d1d6c3eddbdb04cf",
+    "url": "/youtube-mirrorify/static/css/main.ea1be238.chunk.css"
+  },
+  {
+    "revision": "92acca48655fee1b6730",
+    "url": "/youtube-mirrorify/static/js/2.fb803bf0.chunk.js"
+  },
+  {
+    "revision": "f949a1127d8f2991af4a7d26322b4db5",
+    "url": "/youtube-mirrorify/static/js/2.fb803bf0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6be0d1d6c3eddbdb04cf",
+    "url": "/youtube-mirrorify/static/js/main.60d3f6e9.chunk.js"
+  },
+  {
+    "revision": "b393d468ab0778d70dea",
+    "url": "/youtube-mirrorify/static/js/runtime-main.0855d3d0.js"
+  }
+]);
